f561f27867625109f9157a9bb2eb7a56eec6531c
Japheth Obala <japhethobalak@gmail.com>
2019-02-14 19:26:00 +0300

    Adds a test case for checking if a .env file existst.

--- client.py
+++ client.py
@@ -1,3 +1,20 @@
+from pathlib import Path
+from dotenv import load_dotenv
+
+
 class Client:
     def __init__(self, scopes=[]):
-        pass
+        # TODO: Understand kwargs
+        self.config = {}
+        self.load_config()
+
+    def load_config(self):
+        # TODO: Understand how property access works in python
+        env_file = Path('/.env')
+
+        try:
+            env_file.resolve(strict=True)
+        except FileNotFoundError:
+            raise FileNotFoundError('.env file does not exist.')
+        else:
+            load_dotenv()
--- tests/test_initialization.py
+++ tests/test_initialization.py
@@ -4,8 +4,9 @@ from client import Client
 
 class TestInitialization(unittest.TestCase):
 
-    def test_checks_for_config(self):
-        pass
+    def test_throws_an_error_if_no_env(self):
+        with self.assertRaises(FileNotFoundError):
+            Client()
 
     def test_checks_if_config_has_all_required_properties(self):
         pass
